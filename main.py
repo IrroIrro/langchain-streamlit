1bfe5455471cab3c456b80da3690dd43fb26a218
IrroIrro <70427740+IrroIrro@users.noreply.github.com>
2023-08-29 15:01:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -72,29 +72,26 @@ if uploaded_file is not None:
                 embedding=OpenAIEmbeddings(),
                 persist_directory=persist_directory
             )
-        
+
 question = st.text_input("Enter your question:", "Who are the main 3 findings?")
-    if question:
-        template = """Use the following pieces of context to answer the question at the end. 
-        If you don't know the answer, just say that you don't know, don't try to make up an answer. 
-        Use five sentences maximum and provide the most accurate and precise answer.
-        {context}
+if question:
+    template = """Use the following pieces of context to answer the question at the end. 
+    If you don't know the answer, just say that you don't know, don't try to make up an answer. 
+    Use five sentences maximum and provide the most accurate and precise answer.
+    {context}
+    """        
+    QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"], template=template)
 
-        Question: {question}
-        Creative Answer:"""
-        
-        QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"], template=template)
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.8)
+    qa_chain = RetrievalQA.from_chain_type(
+        llm,
+        retriever=vectorstore.as_retriever(),
+        chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
+        return_source_documents=True
+    )
+    result = qa_chain({"query": question})
 
-        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.8)
-        qa_chain = RetrievalQA.from_chain_type(
-            llm,
-            retriever=vectorstore.as_retriever(),
-            chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
-            return_source_documents=True
-        )
-        result = qa_chain({"query": question})
-
-        st.write(f"Answer: {result['result']}")
+    st.write(f"Answer: {result['result']}")
 
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
