5d50719bf0b435f0f7b3bdf9508191101a5e37c9
IrroIrro <70427740+IrroIrro@users.noreply.github.com>
2023-08-29 13:33:30 -0600

    Update main.py
    Uploader added

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 """Python file to serve as the frontend"""
+import PyPDF2
+
 import streamlit as st
 from streamlit_chat import message
 
 from langchain.chains import ConversationChain
 from langchain.llms import OpenAI
 
+def read_pdf(file):
+    pdf_reader = PyPDF2.PdfFileReader(file)
+    text = ""
+    for page_num in range(pdf_reader.numPages):
+        page = pdf_reader.getPage(page_num)
+        text += page.extractText()
+    return text
 
 def load_chain():
     """Logic for loading the chain you want to use should go here."""
-    llm = OpenAI(temperature=0)
+    llm = OpenAI(temperature=0.2)
     chain = ConversationChain(llm=llm)
     return chain
 
@@ -18,17 +27,21 @@ chain = load_chain()
 st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
 st.header("LangChain Demo")
 
+uploaded_file = st.file_uploader("Choose a PDF file", type=["pdf"])
+
+if uploaded_file:
+    pdf_text = read_pdf(uploaded_file)
+    st.text_area("Content of the PDF:", pdf_text, height=300)
+
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
 
 if "past" not in st.session_state:
     st.session_state["past"] = []
 
-
 def get_text():
     input_text = st.text_input("You: ", "Hello, how are you?", key="input")
     return input_text
-
 
 user_input = get_text()
 
@@ -39,7 +52,6 @@ if user_input:
     st.session_state.generated.append(output)
 
 if st.session_state["generated"]:
-
     for i in range(len(st.session_state["generated"]) - 1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
         message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
