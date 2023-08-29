382130871872386151043bbd14266211b614e41b
IrroIrro <70427740+IrroIrro@users.noreply.github.com>
2023-08-29 14:07:21 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """Python file to serve as the frontend"""
 import streamlit as st
+from streamlit_chat import message
+
 from langchain.chains import ConversationChain
 from langchain.llms import OpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
