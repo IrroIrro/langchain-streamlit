ef5b11972859342aba84cb6d05f0df6c5551de97
IrroIrro <70427740+IrroIrro@users.noreply.github.com>
2023-08-29 15:21:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -77,20 +77,30 @@ if uploaded_file is not None:
 
 question = st.text_input("Enter your question:", "Who are the main 3 findings?")
 if question:
-    template = """Use the following pieces of context to answer the question at the end. 
-    If you don't know the answer, just say that you don't know, don't try to make up an answer. 
-    Use five sentences maximum and provide the most accurate and precise answer.
+    template = """Based on the following excerpts from scientific papers, provide an answer to the question that follows.
+    Structure your answer with a minimum of two paragraphs, each containing at least five sentences. Begin by presenting a general overview and then delve into specific details, such as numerical data or particular citations.
+    If the answer is not apparent from the provided context, state explicitly that you don't have the information. 
+    When referencing the content, provide a scientific citation. For instance: (Blom and Voesenek, 1996).
+    If the source is unknown, indicate with "No Source".
+    
+    Context:
     {context}
-    """        
+    
+    Question: 
+    {question}
+    
+    Desired Answer:"""
+    
     QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"], template=template)
 
+    # Run chain
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.8)
-    qa_chain = RetrievalQA.from_chain_type(
-        llm,
-        retriever=vectorstore.as_retriever(),
-        chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
-        return_source_documents=True
-    )
+    
+    qa_chain = RetrievalQA.from_chain_type(llm,
+                                       retriever=vectorstore.as_retriever(),
+                                       chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
+                                       return_source_documents=True)
+    
     result = qa_chain({"query": question})
 
     st.write(f"Answer: {result['result']}")
