1de6a88af5ecb6b624cc8eeea3fa00fc6a46c432
IrroIrro <70427740+IrroIrro@users.noreply.github.com>
2023-09-05 16:55:58 -0600

    Update main.py
    FORK

--- main.py
+++ main.py
@@ -1,117 +1,117 @@
-"""Python file to serve as the frontend"""
-import streamlit as st
-from streamlit_chat import message
-
-from langchain.chains import ConversationChain
-from langchain.llms import OpenAI
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
-from langchain.vectorstores import Chroma
-from langchain.llms import OpenAI
-from langchain.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.prompts import PromptTemplate
-from langchain.document_loaders import PyPDFLoader
 import os
-import uuid
-from collections import namedtuple
-import PyPDF2
-import pickle
-from langchain.vectorstores import FAISS
-import tiktoken
-import numpy as np
+import tempfile
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import PyPDFLoader
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores import DocArrayInMemorySearch
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-# Set Streamlit Config
-st.set_page_config(page_title="ChatGPT for BERA", page_icon=":robot:")
-st.header("ChatGPT for BERA")
+st.set_page_config(page_title="BERA: Chat with Documents", page_icon="🦜")
+st.title("🦜 BERA: Chat with Documents")
 
-# Create a placeholder for the content
-content_placeholder = st.empty()
+# Store the PDFs in a list for selection
+available_pdfs = ['document1.pdf', 'document2.pdf']  # Example list
 
-# Extend the Document structure to include a metadata attribute
-Document = namedtuple("Document", ["page_content", "metadata"])
+# Sidebar to upload or select files
+st.sidebar.header('Document Source')
+uploaded_files = st.sidebar.file_uploader(
+    label="Upload PDF files", type=["pdf"], accept_multiple_files=True
+)
+selected_files = st.sidebar.multiselect("Or select from existing PDFs:", available_pdfs)
 
-def read_pdf(file, file_path):
-    pdf_reader = PyPDF2.PdfReader(file)
-    pages_content = []
-    for page_num in range(len(pdf_reader.pages)):
-        page = pdf_reader.pages[page_num]
-        metadata = {'source': file_path, 'page': page_num + 1}
-        pages_content.append(Document(page_content=page.extract_text(), metadata=metadata))
-    return pages_content
+if not uploaded_files and not selected_files:
+    st.info("Please upload or select PDF documents to continue.")
+    st.stop()
 
-def process_and_create_vectorstore(uploaded_file, file_path):
-    text_splitter = CharacterTextSplitter(separator="\n\n", chunk_size=2000, chunk_overlap=500, length_function=len)
-    pages = read_pdf(uploaded_file, file_path)
-    splits = text_splitter.split_documents(pages)
-    chunk_texts = [chunk.page_content for chunk in splits]
-    embeddings = OpenAIEmbeddings()
-    vectorstore = FAISS.from_texts(texts=chunk_texts, embedding=embeddings)    
-    return vectorstore
+def configure_retriever(uploaded_files):
+    docs = []
+    temp_dir = tempfile.TemporaryDirectory()
+    for file in uploaded_files:
+        temp_filepath = os.path.join(temp_dir.name, file.name)
+        with open(temp_filepath, "wb") as f:
+            f.write(file.getvalue())
+        loader = PyPDFLoader(temp_filepath)
+        docs.extend(loader.load())
 
-# Initialize session state if needed
-if "chat_history" not in st.session_state:
-    st.session_state["chat_history"] = []
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.5)
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    vectordb = DocArrayInMemorySearch.from_documents(splits, embeddings)
 
-template = """Based on the following excerpts from scientific papers, provide an answer to the question that follows.
-        Structure your answer with a minimum of two paragraphs, each containing at least five sentences. Begin by presenting a general overview and then delve into specific details, such as numerical data or particular citations.
-        If the answer is not apparent from the provided context, state explicitly that you don't have the information. 
-        When referencing the content, provide a scientific citation. For instance: (Blom and Voesenek, 1996).
-        If the source is unknown, indicate with "No Source".
-        
-        Context:
-        {context}
-        
-        Question: 
-        {question}
-        
-        Desired Answer:"""
+    retriever = vectordb.as_retriever(search_type="mmr", search_kwargs={"k": 2, "fetch_k": 4})
 
-QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"], template=template)
+    return retriever
 
-vectorstore_files = [filename for filename in os.listdir() if filename.startswith("vectorstore_")]
-vectorstore_titles = [filename[len("vectorstore_"):-len(".pkl")] for filename in vectorstore_files]
-selected_title = st.selectbox("Select a stored PDF file:", [""] + vectorstore_titles)
+# ... [Continued from the previous section of the code] ...
 
-if selected_title:
-    selected_filename = f"vectorstore_{selected_title}.pkl"
-    with open(selected_filename, "rb") as f:
-        vectorstore = pickle.load(f)
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container: st.delta_generator.DeltaGenerator, initial_text: str = ""):
+        self.container = container
+        self.text = initial_text
+        self.run_id_ignore_token = None
 
-uploaded_file = st.file_uploader("Or, upload a new PDF file:", type="pdf")
-if uploaded_file:
-    uploaded_file_title = st.text_input("Enter a title for the uploaded PDF file:")
-    if st.button("Process PDF"):
-        virtual_directory = "/virtual_upload_directory"
-        unique_filename = f"{uuid.uuid4()}_{uploaded_file.name}"
-        file_path = os.path.join(virtual_directory, unique_filename)
-        vectorstore = process_and_create_vectorstore(uploaded_file, file_path)
-        vectorstore_filename = f"vectorstore_{uploaded_file_title}.pkl"
-        with open(vectorstore_filename, "wb") as f:
-            pickle.dump(vectorstore, f)
-        st.session_state["chat_history"].append(("System", "PDF processed successfully! You can now ask questions about the document."))
+    def on_llm_start(self, serialized: dict, prompts: list, **kwargs):
+        if prompts[0].startswith("Human"):
+            self.run_id_ignore_token = kwargs.get("run_id")
 
-if vectorstore:
-    qa_chain = RetrievalQA.from_chain_type(llm, retriever=vectorstore.as_retriever(), chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}, return_source_documents=True)      
-    question = st.text_input("Enter your question about the document:")
-    if st.button("Submit Question"):
-        st.session_state["chat_history"].append(("You", question))
-        
-        result = qa_chain({"query": question})
-        st.session_state["chat_history"].append(("ChatGPT", result['result']))
-    
-    # Display chat history using containers
-    for sender, message in st.session_state["chat_history"]:
-        if sender == "You":
-            st.markdown("##### You")
-            st.container().markdown(message, unsafe_allow_html=True)
-        elif sender == "ChatGPT":
-            st.markdown("##### ChatGPT")
-            st.container().markdown(message, unsafe_allow_html=True)
-        else:  # for system messages
-            st.markdown("##### System")
-            st.container().markdown(f"_{message}_", unsafe_allow_html=True)
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        if self.run_id_ignore_token == kwargs.get("run_id", False):
+            return
+        self.text += token
+        self.container.markdown(self.text)
+
+class PrintRetrievalHandler(BaseCallbackHandler):
+    def __init__(self, container):
+        self.status = container.status("**Context Retrieval**")
+
+    def on_retriever_start(self, serialized: dict, query: str, **kwargs):
+        self.status.write(f"**Question:** {query}")
+        self.status.update(label=f"**Context Retrieval:** {query}")
+
+    def on_retriever_end(self, documents, **kwargs):
+        for idx, doc in enumerate(documents):
+            source = os.path.basename(doc.metadata["source"])
+            self.status.write(f"**Document {idx} from {source}**")
+            self.status.markdown(doc.page_content)
+        self.status.update(state="complete")
+
+if not uploaded_files and not selected_files:
+    st.info("Please upload or select PDF documents to continue.")
+    st.stop()
+
+files_to_process = uploaded_files if uploaded_files else selected_files
+retriever = configure_retriever(files_to_process)
+
+# Setup memory for contextual conversation
+msgs = StreamlitChatMessageHistory()
+memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=msgs, return_messages=True)
+
+# Setup LLM and QA chain
+llm = ChatOpenAI(
+    model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, temperature=0, streaming=True
+)
+qa_chain = ConversationalRetrievalChain.from_llm(
+    llm, retriever=retriever, memory=memory, verbose=True
+)
+
+if len(msgs.messages) == 0 or st.sidebar.button("Clear message history"):
+    msgs.clear()
+    msgs.add_ai_message("How can I help you?")
+
+avatars = {"human": "user", "ai": "assistant"}
+for msg in msgs.messages:
+    st.chat_message(avatars[msg.type]).write(msg.content)
+
+if user_query := st.chat_input(placeholder="Ask me anything!"):
+    st.chat_message("user").write(user_query)
+
+    with st.chat_message("assistant"):
+        retrieval_handler = PrintRetrievalHandler(st.container())
+        stream_handler = StreamHandler(st.empty())
+        response = qa_chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
