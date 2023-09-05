0f35535aafdda89327c7e69ca883e23fcfd592a7
IrroIrro <70427740+IrroIrro@users.noreply.github.com>
2023-09-05 16:08:33 -0600

    Update main.py
    back up changes

--- main.py
+++ main.py
@@ -28,14 +28,6 @@ if not hasattr(st.session_state, "initialized"):
     st.set_page_config(page_title="ChatGPT for BERA", page_icon=":robot:")
     st.session_state.initialized = True
 
-def load_chain():
-    llm = OpenAI(temperature=0.2)
-    chain = ConversationChain(llm=llm)
-    return chain
-
-# Load Chain
-chain = load_chain()
-
 # Extend the Document structure to include a metadata attribute
 Document = namedtuple("Document", ["page_content", "metadata"])
 
@@ -49,31 +41,14 @@ def read_pdf(file, file_path):
     return pages_content
 
 def process_and_create_vectorstore(uploaded_file, file_path):
-
-    # Split PDF into chunks
-    text_splitter = CharacterTextSplitter(        
-        separator="\n\n",
-        chunk_size=2000,
-        chunk_overlap=500,
-        length_function=len,
-    )
-    
-    pages = read_pdf(uploaded_file, file_path)  # Assuming file_path is defined earlier
-    
+    text_splitter = CharacterTextSplitter(separator="\n\n", chunk_size=2000, chunk_overlap=500, length_function=len)
+    pages = read_pdf(uploaded_file, file_path)
     splits = text_splitter.split_documents(pages)
-
     chunk_texts = [chunk.page_content for chunk in splits]
-
-    # Embedding (Openai methods)
     embeddings = OpenAIEmbeddings()
-    
-    # Store the chunks part in db (vector)
-    vectorstore = FAISS.from_texts(
-        texts=chunk_texts,  # Pass the extracted text content
-        embedding=embeddings)    
+    vectorstore = FAISS.from_texts(texts=chunk_texts, embedding=embeddings)    
     return vectorstore
     
-# Load QA chain
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.5)
 
 template = """Based on the following excerpts from scientific papers, provide an answer to the question that follows.
@@ -95,22 +70,15 @@ QA_CHAIN_PROMPT = PromptTemplate(input_variables=["context", "question"], templa
 # Initialize session state if needed
 if "generated_qa" not in st.session_state:
     st.session_state["generated_qa"] = []
-
 if "past_qa" not in st.session_state:
     st.session_state["past_qa"] = []
 
-# Display previously stored Q&A
-for i in range(len(st.session_state["generated_qa"])):
-    message(st.session_state["generated_qa"][i], key=f"{i}_generated")
-    message(st.session_state["past_qa"][i], is_user=True, key=f"{i}_user")
-
-# PDF Upload or Selection Logic
 vectorstore = None
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
 if uploaded_file:
     uploaded_file_title = st.text_input("Enter a title for the uploaded PDF file:")
-    if st.button("Process and work with PDF"):
+    if st.button("Process PDF"):
         virtual_directory = "/virtual_upload_directory"
         unique_filename = f"{uuid.uuid4()}_{uploaded_file.name}"
         file_path = os.path.join(virtual_directory, unique_filename)
@@ -118,39 +86,22 @@ if uploaded_file:
         vectorstore_filename = f"vectorstore_{uploaded_file_title}.pkl"
         with open(vectorstore_filename, "wb") as f:
             pickle.dump(vectorstore, f)
-        print('Working with uploaded file')
 else:
     vectorstore_files = [filename for filename in os.listdir() if filename.startswith("vectorstore_")]
     if vectorstore_files:
         vectorstore_titles = [filename[len("vectorstore_"):-len(".pkl")] for filename in vectorstore_files]
         selected_title = st.selectbox("Select a stored PDF file:", [""] + vectorstore_titles)
-        
         if selected_title:
             selected_filename = f"vectorstore_{selected_title}.pkl"
             with open(selected_filename, "rb") as f:
                 vectorstore = pickle.load(f)
-            print('Working with selected file')
 
-# If a vectorstore is available, perform QA
 if vectorstore:
-    
-    # Maintain the state of the current question to persist the question across reruns
-    if "current_question" not in st.session_state:
-        st.session_state.current_question = ""
-
-    st.session_state.current_question = st.text_input("Enter your question about the document:", st.session_state.current_question)
-    
-    # Use a button to trigger the QA process
-    if st.button("Submit Question") and st.session_state.current_question:
-        qa_chain = RetrievalQA.from_chain_type(llm,
-                                               retriever=vectorstore.as_retriever(),
-                                               chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
-                                               return_source_documents=True)  
-        
-        result = qa_chain({"query": st.session_state.current_question})
+    question = st.text_input("Enter your question about the document:")
+    if st.button("Submit Question"):
+        qa_chain = RetrievalQA.from_chain_type(llm, retriever=vectorstore.as_retriever(), chain_type_kwargs={"prompt": QA_CHAIN_PROMPT}, return_source_documents=True)  
+        result = qa_chain({"query": question})
         st.session_state["generated_qa"].append(result['result'])
-        st.session_state["past_qa"].append(st.session_state.current_question)
-        
-        # Display the newly added QA
-        message(result['result'], key=f"{len(st.session_state['generated_qa'])}_generated")
-        message(st.session_state.current_question, is_user=True, key=f"{len(st.session_state['past_qa'])}_user")
+        st.session_state["past_qa"].append(question)
+        st.write("Answer:", result['result'])
+        st.write("Question:", question)
