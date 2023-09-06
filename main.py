8b663fdaaf1a2bcf16e6842e540463a6b04f3989
IrroIrro <70427740+IrroIrro@users.noreply.github.com>
2023-09-06 12:09:16 -0600

    Update main.py

--- main.py
+++ main.py
@@ -149,11 +149,12 @@ if len(msgs.messages) == 0 or st.sidebar.button("Clear message history"):
 avatars = {"human": "user", "ai": "assistant"}
 for msg in msgs.messages:
     st.chat_message(avatars[msg.type]).write(msg.content)
-
+   
 if user_query := st.chat_input(placeholder="Ask me anything!"):
     st.chat_message("user").write(user_query)
-
+    
     with st.chat_message("assistant"):
         retrieval_handler = PrintRetrievalHandler(st.container())
         stream_handler = StreamHandler(st.empty())
-        response = qa_chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
+        # response = qa_chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
+        response = qa_chain.run(user_query)
